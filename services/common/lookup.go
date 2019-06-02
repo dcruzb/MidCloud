1bf079986e912997acb60ce446186ef8b2935812
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-02 00:53:19 -0300

    # Agora um bind com mesmo nome atualiza o registro # Implementada função List para listar todos os serviços registrados

--- services/common/lookup.go
+++ services/common/lookup.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dcbCIn/MidCloud/lib"
 )
 
+// Absolute Object Reference (AOR) implementation.
 type ClientProxy struct {
 	Ip   string
 	Port int
@@ -12,22 +13,34 @@ type ClientProxy struct {
 	ObjectId int
 }
 
+// A record for the name server, to
 type NamingRecord struct {
 	serviceName string
 	clientProxy ClientProxy
 }
 
+// Lookup Remoting Pattern for location transparency
 type ILookup interface {
 	Bind(sn string, cp ClientProxy) (err error)
 	Lookup(serviceName string) (cp ClientProxy, err error)
+	List() []NamingRecord
 }
 
 type Lookup struct {
 	services []NamingRecord
 }
 
+// Binds the name to the AOR.
+// If already exists a service with the same name it is updated
 func (l *Lookup) Bind(sn string, cp ClientProxy) (err error) {
 	lib.PrintlnInfo("Lookup", "Service bind =", sn)
+	for i, nr := range l.services {
+		if nr.serviceName == sn {
+			l.services[i] = NamingRecord{sn, cp}
+			return nil
+		}
+	}
+
 	l.services = append(l.services, NamingRecord{sn, cp})
 	return nil
 }
@@ -40,4 +53,9 @@ func (l Lookup) Lookup(serviceName string) (cp ClientProxy, err error) {
 		}
 	}
 	return ClientProxy{}, nil
+}
+
+func (l Lookup) List() []NamingRecord {
+	lib.PrintlnInfo("Lookup", "Service list (", len(l.services), ")")
+	return l.services
 }
